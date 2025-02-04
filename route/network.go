af1982bc91016edcf2ca167116cfe24baeb96353
GooRingX <gooringx@gmail.com>
2025-02-04 10:21:50 +0800

    Update network.go

--- route/network.go
+++ route/network.go
@@ -16,6 +16,7 @@ import (
 	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/experimental/libbox/platform"
 	"github.com/sagernet/sing-box/option"
+	tun "github.com/sagernet/sing-tun"
 	"github.com/sagernet/sing/common"
 	"github.com/sagernet/sing/common/atomic"
 	"github.com/sagernet/sing/common/control"
