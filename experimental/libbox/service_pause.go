f87eca7fd37e9d08a921252f6ab3c50abed0e389
GooRingX <GooRingX@gmail.com>
2025-02-25 21:16:54 +0800

    pause/wake

--- experimental/libbox/service_pause.go
+++ experimental/libbox/service_pause.go
@@ -14,53 +14,32 @@ func (s *BoxService) Pause() {
 	if s.instance != nil && s.instance.Logger() != nil { //karing
 		s.instance.Logger().Error("BoxService:DevicePause")
 	}
+	s.pauseManager.DevicePause() //karing
+	s.ResetNetwork()             //karing
 
 	s.pauseAccess.Lock()
 	defer s.pauseAccess.Unlock()
 	if s.pauseTimer != nil {
 		s.pauseTimer.Stop()
 	}
-	s.pauseTimer = time.AfterFunc(60*time.Second, s.pause) //karing
+	//s.pauseTimer = time.AfterFunc(3*time.Second, s.ResetNetwork) //karing
 }
 
 func (s *BoxService) Wake() {
 	if s.instance != nil && s.instance.Logger() != nil { //karing
 		s.instance.Logger().Error("BoxService:DeviceWake")
 	}
+	s.pauseManager.DeviceWake() //karing
+	s.ResetNetwork()            //karing
 
 	s.pauseAccess.Lock()
 	defer s.pauseAccess.Unlock()
 	if s.pauseTimer != nil {
 		s.pauseTimer.Stop()
-	} else { //karing
-		if s.instance != nil && s.instance.Logger() != nil { //karing
-			s.instance.Logger().Error("BoxService:DeviceWaked")
-		}
-		s.pauseManager.DeviceWake()
-		s.pauseManager.NetworkWake()
-		s.ResetNetwork()
 	}
 	//s.pauseTimer = time.AfterFunc(3*time.Minute, s.ResetNetwork)//karing
 }
 
 func (s *BoxService) ResetNetwork() {
-	if s.instance != nil && s.instance.Logger() != nil { //karing
-		s.instance.Logger().Error("BoxService:ResetNetwork")
-	}
-
 	s.instance.Router().ResetNetwork()
-}
-
-func (s *BoxService) pause() { //karing
-	s.pauseAccess.Lock()
-	defer s.pauseAccess.Unlock()
-	if s.pauseTimer != nil {
-		s.pauseTimer.Stop()
-	}
-	if s.instance != nil && s.instance.Logger() != nil { //karing
-		s.instance.Logger().Error("BoxService:DevicePaused")
-	}
-	s.pauseManager.DevicePause()
-	s.pauseManager.NetworkPause()
-	s.ResetNetwork()
 }
