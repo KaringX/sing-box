60cf6c5a68709c45335efa7a9d7fae6483b00765
GooRingX <GooRingX@gmail.com>
2025-03-03 14:11:37 +0800

    pause wake

--- experimental/libbox/service_pause.go
+++ experimental/libbox/service_pause.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/common/conntrack"
+	C "github.com/sagernet/sing-box/constant"
 	"github.com/sagernet/sing-box/experimental/clashapi"
 	"github.com/sagernet/sing/service"
 )
@@ -13,6 +14,11 @@ import (
 type servicePauseFields struct {
 	pauseAccess sync.Mutex
 	pauseTimer  *time.Timer
+}
+
+type proxyOutboundUploadAndDownload struct {
+	upload   int64
+	download int64
 }
 
 func (s *BoxService) Pause() {
@@ -43,7 +49,16 @@ func (s *BoxService) Wake() {
 	if s.pauseTimer != nil {
 		s.pauseTimer.Stop()
 	}
+	s.pauseTimer = time.AfterFunc(15*time.Second, s.TryResetNetwork) //karing
 	//s.pauseTimer = time.AfterFunc(3*time.Minute, s.ResetNetwork) //karing
+}
+
+func (s *BoxService) ResetNetwork() {
+	s.instance.Router().ResetNetwork()
+}
+
+func (s *BoxService) UpdateWIFIState() {
+	s.instance.Network().UpdateWIFIState()
 }
 
 func (s *BoxService) getConnectionInAndOutCount() (int, int) { //karing
@@ -60,10 +75,75 @@ func (s *BoxService) getConnectionInAndOutCount() (int, int) { //karing
 	return connectionsIn, connectionsOut
 }
 
-func (s *BoxService) ResetNetwork() {
-	s.instance.Router().ResetNetwork()
+func isProxyOutbound(outboundType string) bool { //karing
+	switch outboundType {
+	case C.TypeSOCKS:
+		return true
+	case C.TypeHTTP:
+		return true
+	case C.TypeShadowsocks:
+		return true
+	case C.TypeVMess:
+		return true
+	case C.TypeTrojan:
+		return true
+	case C.TypeWireGuard:
+		return true
+	case C.TypeHysteria:
+		return true
+	case C.TypeTor:
+		return true
+	case C.TypeSSH:
+		return true
+	case C.TypeShadowTLS:
+		return true
+	case C.TypeShadowsocksR:
+		return true
+	case C.TypeVLESS:
+		return true
+	case C.TypeTUIC:
+		return true
+	case C.TypeHysteria2:
+		return true
+	default:
+		return false
+	}
 }
 
-func (s *BoxService) UpdateWIFIState() {
-	s.instance.Network().UpdateWIFIState()
+func (s *BoxService) TryResetNetwork() { //karing
+	outboundTranffic := make(map[string]proxyOutboundUploadAndDownload)
+	clashServer := service.FromContext[adapter.ClashServer](s.ctx)
+	if clashServer != nil {
+		trafficManager := clashServer.(*clashapi.Server).TrafficManager()
+		if trafficManager != nil {
+			connections := trafficManager.Connections()
+			for _, connection := range connections {
+				if isProxyOutbound(connection.OutboundType) {
+					conn, exist := outboundTranffic[connection.Outbound]
+					if exist {
+						outboundTranffic[connection.Outbound] = proxyOutboundUploadAndDownload{
+							upload:   conn.upload + connection.Upload.Load(),
+							download: conn.download + connection.Download.Load()}
+					} else {
+						outboundTranffic[connection.Outbound] = proxyOutboundUploadAndDownload{
+							upload:   connection.Upload.Load(),
+							download: connection.Download.Load()}
+					}
+				}
+			}
+		}
+	}
+	reset := false
+	for _, outbound := range outboundTranffic {
+		if outbound.upload > 0 && outbound.download == 0 {
+			reset = true
+			break
+		}
+	}
+	if reset {
+		if s.instance != nil && s.instance.Logger() != nil { //karing
+			s.instance.Logger().Error("BoxService:TryResetNetwork")
+		}
+		s.ResetNetwork()
+	}
 }
