1fe67a5397c7f1044afbac7d0149fe35c2fcb401
GooRingX <gooringx@gmail.com>
2025-07-28 18:34:54 +0800

    mieru

--- include/registry.go
+++ include/registry.go
@@ -24,6 +24,7 @@ import (
 	protocolDNS "github.com/sagernet/sing-box/protocol/dns"
 	"github.com/sagernet/sing-box/protocol/group"
 	"github.com/sagernet/sing-box/protocol/http"
+	"github.com/sagernet/sing-box/protocol/mieru"
 	"github.com/sagernet/sing-box/protocol/mixed"
 	"github.com/sagernet/sing-box/protocol/naive"
 	"github.com/sagernet/sing-box/protocol/redirect"
@@ -93,6 +94,7 @@ func OutboundRegistry() *outbound.Registry {
 	shadowtls.RegisterOutbound(registry)
 	vless.RegisterOutbound(registry)
 	anytls.RegisterOutbound(registry)
+	mieru.RegisterOutbound(registry) //karing https://github.com/enfein/mbox
 
 	registerQUICOutbounds(registry)
 	registerWireGuardOutbound(registry)
