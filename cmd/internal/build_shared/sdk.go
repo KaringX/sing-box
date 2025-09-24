4fe31177be11338399736de0890a05fb79165bf5
GooRingX <gooringx@gmail.com>
2025-09-24 17:09:58 +0800

    Update sdk.go

--- cmd/internal/build_shared/sdk.go
+++ cmd/internal/build_shared/sdk.go
@@ -49,7 +49,7 @@ func FindSDK() {
 
 func findNDK() bool {
 	//const fixedVersion =  "28.0.13004108"  //karing
-	const fixedVersion = "27.0.12077973" //karing
+	const fixedVersion = "28.2.13676358" //karing
 
 	const versionFile = "source.properties"
 	if fixedPath := filepath.Join(androidSDKPath, "ndk", fixedVersion); rw.IsFile(filepath.Join(fixedPath, versionFile)) {
