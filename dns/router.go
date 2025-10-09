ca20a221226abae7031a9eb2df3d52733226832a
GooRingX <gooringx@gmail.com>
2025-10-09 16:11:28 +0800

    fix panic

--- dns/router.go
+++ dns/router.go
@@ -292,6 +292,10 @@ func (r *Router) Exchange(ctx context.Context, message *mDNS.Msg, options adapte
 				var responseCheck func(responseAddrs []netip.Addr) bool
 				if rule != nil && rule.WithAddressLimit() {
 					responseCheck = func(responseAddrs []netip.Addr) bool {
+						if responseAddrs == nil { //karing
+							r.logger.InfoContext(ctx, "lookup result is nil for ", metadata.Domain)
+							return true
+						}
 						metadata.DestinationAddresses = responseAddrs
 						return rule.MatchAddressLimit(metadata)
 					}
@@ -433,6 +437,10 @@ func (r *Router) Lookup(ctx context.Context, domain string, options adapter.DNSQ
 			var responseCheck func(responseAddrs []netip.Addr) bool
 			if rule != nil && rule.WithAddressLimit() {
 				responseCheck = func(responseAddrs []netip.Addr) bool {
+					if responseAddrs == nil { //karing
+						r.logger.InfoContext(ctx, "lookup result is nil for ", domain)
+						return true
+					}
 					metadata.DestinationAddresses = responseAddrs
 					return rule.MatchAddressLimit(metadata)
 				}
@@ -574,6 +582,10 @@ func (r *Router) LookupTag(ctx context.Context, domain string, options adapter.D
 			var responseCheck func(responseAddrs []netip.Addr) bool
 			if rule != nil && rule.WithAddressLimit() {
 				responseCheck = func(responseAddrs []netip.Addr) bool {
+					if responseAddrs == nil { //karing
+						r.logger.InfoContext(ctx, "lookup result is nil for ", domain)
+						return true
+					}
 					metadata.DestinationAddresses = responseAddrs
 					return rule.MatchAddressLimit(metadata)
 				}
